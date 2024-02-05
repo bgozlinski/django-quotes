58aa34e967eace57ad39ec5b4191b377d95cdab7
bgozlinski <bartlomiej.gozlinski@gmail.com>
2024-02-05 20:00:36 +0100

    Added new unscrypted buttons, direct links to authors page

--- quoteapp/urls.py
+++ quoteapp/urls.py
@@ -6,6 +6,7 @@ app_name = 'quoteapp'
 urlpatterns = [
     path('', views.main, name='main'),
     path('add_author/', views.add_author, name='add_author'),
-    path('add_quote/', views.add_quote, name='add_quote')
+    path('add_quote/', views.add_quote, name='add_quote'),
+    path('author/<int:author_id>', views.detail_author, name='detail_author'),
 
 ]
--- quoteapp/views.py
+++ quoteapp/views.py
@@ -1,7 +1,4 @@
-from django.core.paginator import Paginator
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-
+from django.shortcuts import render, redirect, get_object_or_404
 from quoteapp.forms import QuoteForm, AuthorForm
 from quoteapp.models import Author, Quote
 
@@ -41,3 +38,9 @@ def add_quote(request):
             return render(request, 'quotes/add_quote.html', {'authors': authors, 'form': form})
 
     return render(request, 'quotes/add_quote.html', {'authors': authors, 'form': QuoteForm()})
+
+
+def detail_author(request, author_id):
+    author = get_object_or_404(Author, pk=author_id)
+    return render(request, 'authors/detail.html', {'author': author})
+
