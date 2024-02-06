846f947fda2b111ec3b80e54d760f949128cd1cc
bgozlinski <bartlomiej.gozlinski@gmail.com>
2024-02-06 18:46:37 +0100

    Settings update with secodn DB - MongoDB

--- quotes/settings.py
+++ quotes/settings.py
@@ -29,9 +29,9 @@ SECRET_KEY = config['SECRET']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
