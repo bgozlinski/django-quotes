fb7d3de4cf0ee6857afb0dc37b81f5798897c3b3
bgozlinski <bartlomiej.gozlinski@gmail.com>
2024-02-22 23:58:04 +0100

    Added automated password reset for Users

--- users/views.py
+++ users/views.py
@@ -4,6 +4,10 @@ from django.contrib.auth import authenticate, login, logout
 from .forms import RegisterForm, LoginForm
 from django.contrib import messages
 
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.views import PasswordResetView
+from django.urls import reverse_lazy
+
 
 # Create your views here.
 def signup_user(request):
@@ -43,6 +47,10 @@ def logout_user(request):
     return redirect(to='quoteapp:main')
 
 
-
-
-
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'registration/password_reset.html'
+    email_template_name = 'registration/password_reset_email.html'
+    html_email_template_name = 'registration/password_reset_email.html'
+    success_url = reverse_lazy('users:password_reset_done')
+    success_message = "An email with instructions to reset your password has been sent to %(email)s."
+    subject_template_name = 'registration/password_reset_subject.txt'
